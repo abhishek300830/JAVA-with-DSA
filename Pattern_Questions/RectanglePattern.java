1fb9a4a6c98a93827fea65b29b8ab6af6a9949ba
Abhishek Kumar <abhishek300830@gmail.com>
2021-12-13 14:38:44 +0530

    2nd Commit

---
+++ Pattern_Questions/RectanglePattern.java
@@ -0,0 +1,14 @@
+package Pattern_Questions;
+// creating the rectangle pattern from stars of width 5 and height 4.
+public class RectanglePattern {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+            
+    }
+}
